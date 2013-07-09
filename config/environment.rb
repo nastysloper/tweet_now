cd1b03918a4ddc3096e307c3b10dff5f9331113d
Richard Vogt <richvogtjr@gmail.com>
2013-07-09 13:20:34 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,10 @@
 # See: http://gembundler.com/bundler_setup.html
 #      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-ENV['TWITTER_CONSUMER_KEY'] = 'UgGb3fhYXMH5fjsUXBj8w'
-ENV['TWITTER_CONSUMER_SECRET'] = 'wVi3dBst2dqONDbP8CrE9ktgnJbwLoz4GspCvOaCTPs'
-ENV['TWITTER_OAUTH_TOKEN'] = '217263553-1kCTeKe4msOyuj9YX4slLlb08nkTD5G4Fl0NSaDY'
-ENV['TWITTER_OAUTH_TOKEN_SECRET'] = 'FyOPmI2kgX4Ss7ewrIRYaZQ1WhvV5Wf4rlwY4dZkIYU'
+ENV['TWITTER_CONSUMER_KEY'] = ''
+ENV['TWITTER_CONSUMER_SECRET'] = ''
+ENV['TWITTER_OAUTH_TOKEN'] = ''
+ENV['TWITTER_OAUTH_TOKEN_SECRET'] = ''
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
